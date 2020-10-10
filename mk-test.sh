bcd7118eed515b98e154072e3238ef5ace2f0145
paaguti <paaguti@gmail.com>
2020-10-10 08:33:22 +0200

    Provide a small script for quick test compilations

---
+++ mk-test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./configure --enable-dired --enable-pipein --enable-prefixregion \
+  --enable-pythonmode --enable-clikemode \
+  --enable-usermodes  --enable-usermacros \
+  --enable-searchall
+
+make
