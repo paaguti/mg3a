401f639a08ba3d5d6e439f8e9a51e766dd1c217a
Pedro A. Aranda <paaguti@gmail.com>
2025-01-11 09:12:53 +0100

    Cleanup and fix release date

--- mk-test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-make clean
-
-./configure --enable-dired --enable-pipein --enable-prefixregion \
-  --enable-pythonmode --enable-clikemode \
-  --enable-usermodes  --enable-usermacros \
-  --enable-searchall  # --disable-mouse
-
-make
